d423d3ffb1e58c980ae22ffda68576a865536e18
Brandon Nathan Marks <bmarks7@uwo.ca>
2021-11-22 23:18:28 -0500

    added more navbar styling and started about page

--- uwo-csus-site/src/Pages/JS/Home.js
+++ uwo-csus-site/src/Pages/JS/Home.js
@@ -1,11 +1,10 @@
 // The Home page, the first page that users will see
 import React from 'react';
-import Navbar from '../Components/JS/Navbar.js';
+
 
 const Home = () => {
     return ( 
         <div>
-            <Navbar/>
         </div>
     );
 }
